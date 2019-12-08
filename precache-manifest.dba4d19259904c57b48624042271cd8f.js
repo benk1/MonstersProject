9f9464d70ff73d1648934e92448153dc5f5c1700
benk1 <kakengi_bernard@hotmail.com>
2019-12-08 11:16:53 +0200

    Updates

---
+++ precache-manifest.dba4d19259904c57b48624042271cd8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182091bf260120d0acc438c8ac00cb38",
+    "url": "/MonstersProject/index.html"
+  },
+  {
+    "revision": "562f529d58782fd321d0",
+    "url": "/MonstersProject/static/css/main.31b47cfc.chunk.css"
+  },
+  {
+    "revision": "e5a999d069279a8106e4",
+    "url": "/MonstersProject/static/js/2.d580fc4f.chunk.js"
+  },
+  {
+    "revision": "562f529d58782fd321d0",
+    "url": "/MonstersProject/static/js/main.57a67b44.chunk.js"
+  },
+  {
+    "revision": "9c9c32a141f3581085bf",
+    "url": "/MonstersProject/static/js/runtime-main.6e06fc56.js"
+  }
+]);
