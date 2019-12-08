0b65257755645149967530f58ca1b881fca9d0ae
benk1 <kakengi_bernard@hotmail.com>
2019-12-08 06:58:06 +0200

    add searchBar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 import './App.css';
 import CardList from './components/card-list/Card-list.component';
+
+import SearchBox from './components/SearchBox';
 const apiEndPoint = 'https://jsonplaceholder.typicode.com/users';
 class App extends Component {
   state = {
-    monsters: []
+    monsters: [],
+    searchField: ''
   };
   async componentDidMount() {
     const { data: monsters } = await axios.get(apiEndPoint);
@@ -16,12 +19,32 @@ class App extends Component {
     });
   }
 
+  handleChange = e => {
+    this.setState({
+      searchField: e.target.value.toLowerCase()
+    });
+  };
+
   render() {
-    const { monsters } = this.state;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField)
+    );
 
     return (
       <div className='App'>
-        <CardList monsters={monsters} />
+        <input
+          className='search'
+          type='search'
+          placeholder='Search Monsters'
+          name='searchInput'
+          onChange={this.handleChange}
+          value={searchField}
+        />
+        <p className='blue-text'>
+          Sum of available Monsters are: {filteredMonsters.length}
+        </p>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card-list/Card-list.component.jsx
+++ src/components/card-list/Card-list.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Card from '../card/Card.component';
 const CardList = ({ monsters }) => {
+    
   const resultsOfMonsters = monsters.map(monster => (
     <Card key={monster.id} monster={monster} />
   ));
